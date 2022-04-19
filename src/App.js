ef6feaa65b735d2cd5bb93153070c5d0829355c7
whddnjs1715 <whddnjs1715@gmail.com>
2022-04-20 00:04:34 +0900

    리스트 UI 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 const App = () => {
+  let posts = '강남 고기 맛집';
   return (
     <div className="App">
       <div className="black-nav">
-        <div>Project</div>
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{ posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
       </div>
     </div>
   )
