527cfe72505323b687d7927e3b5cc04822cdf0c1
whddnjs1715 <whddnjs1715@gmail.com>
2022-04-21 14:24:27 +0900

    설정 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 const App = () => {
-  let posts = '강남 고기 맛집';
+  let posts = '개발 블로그';
   return (
     <div className="App">
       <div className="black-nav">
@@ -9,7 +9,7 @@ const App = () => {
       </div>
       <div className="list">
         <h3>{posts}</h3>
-        <p>2월 17일 발행</p>
+        <p>4월 20일 발행</p>
         <hr />
       </div>
     </div>
