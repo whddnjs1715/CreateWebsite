d6d3a11dfe6ddf8841d23b9c5c75a3ca889cfbae
whddnjs1715 <whddnjs1715@gmail.com>
2022-04-23 23:52:42 +0900

    useState 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
-  let posts = '개발 블로그';
+  const posts = '개발 블로그';
+  const [name, setName] = useState('언어 추천');
+  const [num, setNum] = useState([1, 2, 3]);
   return (
     <div className="App">
       <div className="black-nav">
@@ -9,7 +12,10 @@ const App = () => {
       </div>
       <div className="list">
         <h3>{posts}</h3>
+        <h4>{num[0]}</h4>
+        <h4>{setNum[(num[0] = 4)]}</h4>
         <p>2022년 4월 22일 발행</p>
+        <h4>{num}</h4>
         <hr />
       </div>
     </div>
