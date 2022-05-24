92aaf69990c2cf4ac6a7521cc79c3f471cabe0f9
whddnjs1715 <whddnjs1715@gmail.com>
2022-05-24 13:59:15 +0900

    #3 이미지 사용 & public 폴더 사용_2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import bg from './img/bg.png';
 
 function App() {
   return (
@@ -14,6 +15,25 @@ function App() {
         </Container>
       </Navbar>
       <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
