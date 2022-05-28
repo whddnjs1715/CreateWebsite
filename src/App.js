1f10d227e97a6c18ffbb4ef304f2264b3b08f88b
whddnjs1715 <whddnjs1715@gmail.com>
2022-05-28 23:55:29 +0900

    #3 data.js 추가_4

--- src/App.js
+++ src/App.js
@@ -9,21 +9,18 @@ function App() {
     const [shoes] = useState(data);
     return (
       <>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
-          <h4>{shoes[0].title}</h4>
-          <p>{shoes[0].content}</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
-          <h4>{shoes[1].title}</h4>
-          <p>{shoes[1].content}</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
-          <h4>{shoes[2].title}</h4>
-          <p>{shoes[2].content}</p>
-        </div>
+        {shoes.map(function (a, i) {
+          return (
+            <div className="col-md-4">
+              <img
+                src={'https://codingapple1.github.io/shop/shoes' + (i + 1) + '.jpg'}
+                width="80%"
+              ></img>
+              <h4>{shoes[i].title}</h4>
+              <p>{shoes[i].content}</p>
+            </div>
+          );
+        })}
       </>
     );
   };
@@ -42,21 +39,6 @@ function App() {
       <div className="container">
         <div className="row">
           <Table />
-          {/* <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].content}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].content}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].content}</p>
-          </div> */}
         </div>
       </div>
     </div>
