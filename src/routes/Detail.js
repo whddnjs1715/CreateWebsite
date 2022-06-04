d945b8388b50040bd706ffd573b1dd2c5c71c730
whddnjs1715 <whddnjs1715@gmail.com>
2022-06-04 18:23:18 +0900

    #4 리액트 라우터_10

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,24 +1,28 @@
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
-  const params = useParams();
+  const { id } = useParams();
   console.log();
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src={'https://codingapple1.github.io/shop/shoes' + (parseInt(params.id) + 1) + '.jpg'}
-            width="100%"
-          />
+      {id < 3 ? (
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src={'https://codingapple1.github.io/shop/shoes' + (parseInt(props.id) + 1) + '.jpg'}
+              width="100%"
+            />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{props.shoes[props.id].title}</h4>
+            <p>{props.shoes[props.id].content}</p>
+            <p>{props.shoes[props.id].price}</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
         </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[params.id].title}</h4>
-          <p>{props.shoes[params.id].content}</p>
-          <p>{props.shoes[params.id].price}</p>
-          <button className="btn btn-danger">주문하기</button>
-        </div>
-      </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
