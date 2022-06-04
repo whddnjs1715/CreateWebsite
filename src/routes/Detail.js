7c1dca08d6079c8e0c5ba59b09679a3a38231cee
whddnjs1715 <whddnjs1715@gmail.com>
2022-06-05 07:26:31 +0900

    #5 styled-components 추가_1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,21 +2,21 @@ import { useParams } from 'react-router-dom';
 
 function Detail(props) {
   const { id } = useParams();
-  console.log();
+  console.log('id : ' + id);
   return (
     <div className="container">
       {id < 3 ? (
         <div className="row">
           <div className="col-md-6">
             <img
-              src={'https://codingapple1.github.io/shop/shoes' + (parseInt(props.id) + 1) + '.jpg'}
+              src={'https://codingapple1.github.io/shop/shoes' + (parseInt(id) + 1) + '.jpg'}
               width="100%"
             />
           </div>
           <div className="col-md-6">
-            <h4 className="pt-5">{props.shoes[props.id].title}</h4>
-            <p>{props.shoes[props.id].content}</p>
-            <p>{props.shoes[props.id].price}</p>
+            <h4 className="pt-5">{props.shoes[id].title}</h4>
+            <p>{props.shoes[id].content}</p>
+            <p>{props.shoes[id].price}</p>
             <button className="btn btn-danger">주문하기</button>
           </div>
         </div>
