225cf13e1cc29c0e3abc86ae3417c0565cc43ef9
whddnjs1715 <whddnjs1715@gmail.com>
2022-07-13 13:53:52 +0900

    #11 장바구니 페이지_3

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './service/store';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
