46c1dc559eaccc1d06218361d6907b9889eb3fad
whddnjs1715 <whddnjs1715@gmail.com>
2022-07-18 09:41:07 +0900

    #11 장바구니 페이지_7

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -2,14 +2,13 @@ import { Table } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
 const Cart = () => {
-  const stock = useSelector((state) => {
+  const cart = useSelector((state) => {
     return state;
   });
   // 축약버전도 가능
   // const a = useSelector((state) => {
   //  state.stock;
   // });
-  console.log(stock.stock[0].name);
 
   return (
     <div>
@@ -23,12 +22,12 @@ const Cart = () => {
           </tr>
         </thead>
         <tbody>
-          {stock.stock.map(function (a, i) {
+          {cart.cart.map(function (a, i) {
             return (
-              <tr>
-                <td>{stock.stock[i].id}</td>
-                <td>{stock.stock[i].name}</td>
-                <td>{stock.stock[i].count}</td>
+              <tr key={i}>
+                <td>{cart.cart[i].id}</td>
+                <td>{cart.cart[i].name}</td>
+                <td>{cart.cart[i].count}</td>
                 <td>변경</td>
               </tr>
             );
--- src/service/store.js
+++ src/service/store.js
@@ -3,21 +3,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 // useState 역할
 // state 하나를 slice라고 부름
-const stock = createSlice({
-  name: 'stock',
+const cart = createSlice({
+  name: 'cart',
   initialState: [
     { id: 0, name: 'White and Black', count: 2 },
     { id: 2, name: 'Grey Yordan', count: 1 },
   ],
 });
 
-// [
-//   {id : 0, name : 'White and Black', count : 2},
-//   {id : 2, name : 'Grey Yordan', count : 1}
-// ]
-
 export default configureStore({
   reducer: {
-    stock: stock.reducer,
+    cart: cart.reducer,
   },
 });
